ebc2591ad7796329732863a8800599027dcf3667
Juan Ignacio Díaz <diazjignacio@gmail.com>
2023-09-05 18:52:35 -0300

    ej. 8

--- server/common/server.py
+++ server/common/server.py
@@ -1,5 +1,6 @@
 import socket
 import logging
+import threading
 
 import common.utils as utils
 
@@ -13,7 +14,8 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
-        self.agencies = []
+        self.barrier = threading.Barrier(AGENCY_COUNT, action=self.filter_winners)
+        self.bets_lock = threading.Lock()
 
     def __enter__(self):
         return self
@@ -25,42 +27,40 @@ class Server:
 
     def close(self):
         self._server_socket.close()
-        for client_sock, _ in self.agencies:
-            client_sock.close()
 
     def run(self):
         """
-        Dummy Server loop
-
-        Server that accept a new connections and establishes a
-        communication with a client. After client stops sending data,
-        server starts to accept new connections again. When all clients
-        are done, server finds winners.
+        Server that accept a new connections and establishes a communication
+        with a client.
         """
-        send = lambda writer, block: writer.send(block)
-
-        while len(self.agencies) < AGENCY_COUNT:
+        workers = []
+        for _ in range(AGENCY_COUNT):
             client_sock = self.__accept_new_connection()
-            try:
-                self.agencies.append((client_sock, self.__handle_client_connection(client_sock)))
-            except (OSError, PartialDataError):
-                logging.info(f"action: connection | result: fail | error: {e}")
-                return
+            t = threading.Thread(target=self.wrap_handler, args=(client_sock,))
+            t.start()
+            workers.append(t)
 
-        logging.info("action: sorteo | result: success")
-        winners = [(bet.agency, bet.document) for bet in utils.load_bets() if utils.has_won(bet)]
-        for client_sock, agency in self.agencies:
-            msg = ''.join(bet[1]+'\n' for bet in winners if bet[0] == agency)
-            msg = msg.encode('utf-8')
-            if utils.try_write(send, client_sock, msg) != len(msg):
-                logging.error(f"action: send_response | result: fail | agency: {agency} | error: short write")
-            else:
-                logging.info(f"action: send_response | result: success | agency: {agency}")
+        for w in workers:
+            w.join()
+
+
+    def wrap_handler(self, client_sock):
+        """
+        Handle resources used by client connection handler
+        """
+        try:
+            self.__handle_client_connection(client_sock)
+        except (OSError, PartialDataError):
+            self.barrier.abort()
+            logging.info(f"action: connection | result: fail | error: {e}")
+        finally:
+            client_sock.close()
 
 
     def __handle_client_connection(self, client_sock):
         """
-        Receive batches of Bets from an agency and store them accordingly
+        Receive batches of Bets from an agency and store them accordingly.
+        When all clients are done, send winners.
         """
         send = lambda writer, block: writer.send(block)
         recv = lambda reader, n: reader.recv(n)
@@ -83,7 +83,8 @@ class Server:
                     bets.append(bet)
                     consumed += n
 
-                utils.store_bets(bets)
+                with self.bets_lock:
+                    utils.store_bets(bets)
 
                 msg = b"success"
                 if utils.try_write(send, client_sock, msg) != len(msg):
@@ -96,7 +97,17 @@ class Server:
             raise
 
         logging.info(f"action: apuesta_almacenada | result: success")
-        return int.from_bytes(buffer[2:], "big")
+        agency = int.from_bytes(buffer[2:], "big")
+
+        # wait for all agencies to finish sending bets and for winners to be filtered
+        self.barrier.wait()
+
+        msg = ''.join(bet[1]+'\n' for bet in self.winners if bet[0] == agency)
+        msg = msg.encode('utf-8')
+        if utils.try_write(send, client_sock, msg) != len(msg):
+            logging.error(f"action: send_response | result: fail | agency: {agency} | error: short write")
+        else:
+            logging.info(f"action: send_response | result: success | agency: {agency}")
 
 
     def __accept_new_connection(self):
@@ -112,3 +123,12 @@ class Server:
         c, addr = self._server_socket.accept()
         logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
         return c
+
+
+    def filter_winners(self):
+        """
+        begin raffle and select winners.
+        should only be called once after the server isn't taking any more bets.
+        """
+        self.winners = [(bet.agency, bet.document) for bet in utils.load_bets() if utils.has_won(bet)]
+        logging.info("action: sorteo | result: success")
